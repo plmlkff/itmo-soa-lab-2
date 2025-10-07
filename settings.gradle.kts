ca72632318d52847cb2f586a25ca1fa8a2158f8e
pa.malkov <pa.malkov@vkteam.ru>
2025-10-08 00:32:32 +0300

    master feat: Implemented project gradle structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "itmo-soa-lab-2"
+
+include(":common")
+project(":common").projectDir = file("common")
+
+include(":product")
+project(":product").projectDir = file("product")
+
+include(":ebay")
+project(":ebay").projectDir = file("ebay")
